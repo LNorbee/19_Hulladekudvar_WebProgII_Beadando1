db5a5dc0bcef93702c11f5a5ef60b37ca99b463e
LNorbee <lazar.norbeert@gmail.com>
2022-10-26 16:36:25 +0200

    nyitolap_main editing

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -1,5 +1,132 @@
 <h2>
-    <br>Üdvözöljük az új rendszerünkben!<br>
-    <br>Jó munkát kívánunk a rendszerünkkel!<br>
-    <br>Kezdhet fgsdfsdfsd...<br>
+    <br><b>Üdvözöljük az új rendszerünkben!</b><br>
 </h2>
+
+<article>
+    <p>
+        A <b>Hulladék Udvar Kft.</b> a lakosság környezetszemléletű
+        tudat-formálását célozva hulladékudvarokat üzemeltet.
+    </p>
+    <p>
+        Hulladékudvarainkba azokat a hulladékokat érdemes elvinni, amelyeket a házhoz menő
+        rendszerben nem szállítanak el, vagy nem adhatók le gyűjtőszigeteken.
+        Ilyenek például a veszélyes hulladékok (pl. festékek, bontási törmelék,
+        akkumulátorok), az elektronikai hulladékok (pl. háztartási kis- és
+        nagygépek, fénycsövek) vagy a háztartásban keletkező lomok.
+    </p>
+    <p>
+        Arról, hogy milyen típusú hulladékot mely hulladékudvarunkba lehet elvinni,
+        a <b><a href="regisztracio" class= "text-success">regisztrációt</a></b> követően kap tájékoztatást
+    </p>
+
+</article>
+<article>
+    <p>
+    <h4><b>Szelektív hulladékgyűjtés</b></h4>
+    </p>
+
+    <p><b>"A környezettudatos életmód a szelektív hulladékgyűjtéssel kezdődik."</b>
+    </p>
+    <p>
+        Már ezzel az apró lépéssel is hozzá tudunk járulni bolygónk
+        (és nem mellesleg az emberiség) megmentéséhez, és egy zöldebb,
+        fenntarthatóbb, a környezetünkkel harmóniában megvalósuló élet
+        kialakításához.
+        A szelektív hulladékgyűjtés a körforgásos gazdaság irányába való
+        elmozdulás egyik fontos alappillére. Az elmúlt években az Európai Unió
+        egyre határozottabb lépéseket tesz a lineáris gazdasági modellről a
+        körkörös gazdasági modellre történő váltásra.
+    </p>
+    <img src="<?=SITE_ROOT?>/assets/images/circular-economy.jpg" class="rounded mx-auto d-block" alt="korkoros_gazdasag" style="height: auto; width: 70%;"/>
+
+</article>
+<article>
+    <p>
+    <h4><b>A szelektív hulladékgyűjtés szabályai:</b></h4>
+    </p>
+    <p>
+        Ahhoz, hogy a hulladékot újra lehessen hasznosítani, az eltérő
+        anyagú hulladékokat külön-külön kell gyűjteni, így a fémet,
+        az üveget, a műanyagot, a papírt, a szerves eredetű,
+        illetve veszélyes hulladékot, de a lomot, a textil-,
+        az elektromos- és elektronikai hulladékot is.
+    </p>
+
+    <ol>
+        <li>
+            A hulladékot tisztán (kiöblítve, letörölve, a szennyezett részeket eltávolítva)
+            helyezzük a megfelelő tartályba! Ha szennyezett hulladék kerül a gyűjtőbe,
+            az egyrészt fertőzésveszéllyel járhat, másrészt beszennyezheti a többi hulladékot is,
+            így veszélyeztetve az újrahasznosított anyag minőségét.
+        </li>
+        <li>
+            A nagyobb hulladékokat (pl. PET palack) nyomjuk össze! Így helyet takarítunk
+            meg és több hulladék fér el a konténerben.
+        </li>
+        <li>
+            A kupakokat csavarjuk le, a fedőket vegyük le és azokat külön dobjuk a szelektív gyűjtőbe!
+        </li>
+        <li>
+            A tejes és gyümölcsleves dobozokat öblítsük el és helyezzük a megfelelő tárolóba! Budapesten
+            ezeket is a papírkonténerbe kell tenni, a kupakokat pedig a műanyaghulladékhoz. Az ország más
+            részein több helyen a dobozokat is a műanyaghulladékokkal együtt kell gyűjteni, ezért minden
+            esetben a helyi hulladékkezelő szolgáltatónál érdemes érdeklődnünk.
+        </li>
+        <li>
+            Azokat a termékeket, amik egyszerre tartalmaznak papírt, fémet és/vagy műanyagot,
+            bontsuk elemeire és elemenként helyezzük a megfelelő tartályba!
+        </li>
+        <li>
+            A papírhulladékot ne műanyagzacskóban dobjuk ki és ne kössük át madzaggal vagy műanyag zsinórral!
+        </li>
+        <li>
+            Attól még, hogy valami műanyag, nem biztos, hogy újrahasznosítható! Figyeljünk a csomagoláson látható jelzésekre!
+        </li>
+        <li>
+            Azokat a hulladékokat, amiket nem lehet az otthoni hulladékgyűjtőbe tenni, vigyük el a legközelebbi
+            szelektív hulladékgyűjtő szigetre vagy hulladékudvarba!
+        </li>
+    </ol>
+    <img src="<?=SITE_ROOT?>/assets/images/szelektiv_hulladek.jpg"  class="rounded mx-auto d-block" alt="szelektiv_hulladek" style="height: auto; width: 70%;"/>
+</article>
+<article>
+    <p>
+    <h4><b>Mi <b>NEM</b> gyűjthető szelektíven</b></h4>
+    </p>
+
+    <h5><b>Semmiképp se kerüljön a papírkonténerbe:</b></h5>
+    <ul>
+        <li>zsíros, szennyezett, ételmaradékos papír</li>
+        <li>hőpapír (blokk, nyugta)</li>
+        <li>közlekedési jegyek</li>
+        <li>sorszámok (bankban, orvosi rendelőben kapott cetlik)</li>
+        <li>indigó</li>
+        <li>faxpapír</li>
+        <li>használt egészségügyi papír (zsebkendő, szalvéta)</li>
+        <li>műanyagréteggel bevont papírcsomagolás (elviteles kávés pohár)</li>
+    </ul>
+
+    <h5><b>Semmiképp se kerüljön a műanyagos konténerbe:</b></h5>
+    <ul>
+        <li>kazetta, CD</li>
+        <li>műanyagjátékok</li>
+        <li>fogkefe</li>
+        <li>fémbelsejű fóliák (chipscsomagolás, csokipapír)</li>
+        <li>habtálca</li>
+        <li>hungarocell</li>
+        <li>ételmaradékokkal vagy vegyszerrel szennyezett, elöblítetlen műanyag és konzervdoboz</li>
+        <li>gyógyszeres levél</li>
+    </ul>
+
+    <h5><b>Semmiképp se kerüljön az üveggyűjtő konténerbe:</b></h5>
+    <ul>
+        <li>villanykörte</li>
+        <li>szemüveg</li>
+        <li>neoncső</li>
+        <li>hőálló üvegtál</li>
+        <li>orvosságos és veszélyes anyagokat tartalmazó üvegek</li>
+    </ul>
+    <img src="<?=SITE_ROOT?>/assets/images/thx-for-recycling.jpeg" class="rounded mx-auto d-block" alt="recycling" style="height: auto; width: 50%"/>
+</article>
+
+<h6><a href="https://xforest.hu/szelektiv-hulladekgyujtes/">Eredeti forrás</a></h6>
