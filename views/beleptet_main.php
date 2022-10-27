ef2c51dbc86fc4638021ebf417fe044c22d27e1c
AhrefREL <bzb6in@hallgato.uni-neumann.hu>
2022-10-27 21:56:50 +0200

    add SOAP client and server, and use with database, wsdl generated, create soap test php

--- views/beleptet_main.php
+++ views/beleptet_main.php
@@ -3,7 +3,6 @@
 <?php if($_SESSION["felhasznaloId"] != "") { ?>
     <a href="<?php echo SITE_ROOT."hulladekudvarkereso"?>" 
 class="btn btn-info" role="button">Milyen szemetet szeretne kidobni?</a>
-
 <?php } else {?>
     <h3>Regisztrálja magát, ha még nem felhasználó!</h3>
 <a href="<?php echo SITE_ROOT."regisztracio"?>" class="btn btn-info" role="button">Regisztráció</a>
